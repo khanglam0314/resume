2f571610bdc0f0dc3cb59cf68737d4c11b70aca7
khanglam0314 <khanglam0314@yahoo.com>
2016-11-21 19:00:27 -0800

    first commit

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -4,30 +4,29 @@ myApp.controller('AppCtrl', ['$scope', '$http',
 		function($scope, $http){
 			console.log("Hello World from controller");
 
-		var refresh = function() {
-		  $http.get('/contactlist').success(function(response) {
+		var refresh1 = function() {
+		  $http.get('/resume/edu').success(function(response) {
 		    console.log("I got the data I requested");
-		    $scope.contactlist = response;
-		    $scope.contact = "";
+		    $scope.resume1 = response;
 		  });
 		};
-		refresh();
+		refresh1();
 
-		$scope.addContact = function() {
-		  console.log($scope.contact);
-		  $http.post('/contactlist', $scope.contact).success(function(response) {
-		    console.log(response);
-		    refresh();
+		var refresh2 = function() {
+		  $http.get('/resume/skill').success(function(response) {
+		    console.log("I got the data I requested");
+		    $scope.resume2 = response;
 		  });
 		};
+		refresh2();
 
-		$scope.remove = function(id) {
-		  console.log(id);
-		  $http.delete('/contactlist/' + id).success(function(response) {
-		    refresh();
+		var refresh3 = function() {
+		  $http.get('/resume/project').success(function(response) {
+		    console.log("I got the data I requested");
+		    $scope.resume3 = response;
 		  });
 		};
-
+		refresh3();
 }]);
 
 // any thing in controller.js running inside the browser
--- server.js
+++ server.js
@@ -1,57 +1,34 @@
-// MEAN Stack RESTful API Tutorial - Contact List App
-
 var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
-var db = mongojs('contactlist', ['contactlist']);
-var bodyParser = require('body-parser');
+var db = mongojs('resume', ['resume']);
 
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
 
-app.get('/contactlist', function (req, res) {
+app.get('/resume/edu', function (req, res) {
   console.log('I received a GET request');
-
-  db.contactlist.find(function (err, docs) {
+  db.resume.find(({type: 'edu' }), function (err, docs) {
     console.log(docs);
     res.json(docs);
   });
 });
 
-app.post('/contactlist', function (req, res) {
-  console.log(req.body);
-  db.contactlist.insert(req.body, function(err, doc) {
-    res.json(doc);
+app.get('/resume/skill', function (req, res) {
+  console.log('I received a GET request');
+  db.resume.find(({type: 'skill'}), function (err, docs) {
+    console.log(docs);
+    res.json(docs);
   });
 });
 
-app.delete('/contactlist/:id', function (req, res) {
-  var id = req.params.id;
-  console.log(id);
-  db.contactlist.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
-    res.json(doc);
+app.get('/resume/project', function (req, res) {
+  console.log('I received a GET request');
+  db.resume.find(({type: 'project'}), function (err, docs) {
+    console.log(docs);
+    res.json(docs);
   });
 });
 
-app.get('/contactlist/:id', function (req, res) {
-  var id = req.params.id;
-  console.log(id);
-  db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
-    res.json(doc);
-  });
-});
 
-app.put('/contactlist/:id', function (req, res) {
-  var id = req.params.id;
-  console.log(req.body.name);
-  db.contactlist.findAndModify({
-    query: {_id: mongojs.ObjectId(id)},
-    update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
-    new: true}, function (err, doc) {
-      res.json(doc);
-    }
-  );
-});
-
-app.listen(3000);
-console.log("Server running on port 3000");
+app.listen(8080);
+console.log("Server running on port 8080");
